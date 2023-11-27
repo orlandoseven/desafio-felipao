ca6640504ddafeb78d9f8fd5d9d726c97ffbf635
orlandoseven <lp.ronnaldo@gmail.com>
2023-11-27 20:25:00 -0300

    projeto criado

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
-console.log("Olá,Mundo")
+let nome = "Flash"
+let xp = 2001
+
+if(xp < 1000){
+    let nivel = "Ferro"
+    console.log("O heroi de nome " + nome + ",esta no nivel " + nivel)    
+}
+
+else if (xp >= 1001 && xp <= 2000){
+    let nivel = "Bronze"
+    console.log("O heroi de nome " + nome + ",esta no nivel " + nivel)    
+}
+
+else if (xp >= 2001 && xp <= 6000){
+    let nivel = "Prata"
+    console.log("O heroi de nome " + nome + ",esta no nivel " + nivel)    
+}
+
+else if (xp >= 6001 && xp <= 7000){
+    let nivel = "Ouro"
+    console.log("O heroi de nome " + nome + ",esta no nivel " + nivel)    
+}
+
+else if (xp >= 7001 && xp <= 8000){
+    let nivel = "Platina"
+    console.log("O heroi de nome " + nome + ",esta no nivel " + nivel)    
+}
+
+else if (xp >= 8001 && xp <= 9000){
+    let nivel = "Ascendente"
+    console.log("O heroi de nome " + nome + ",esta no nivel " + nivel)    
+}
+
+else if (xp >= 9001 && xp <= 10000){
+    let nivel = "Imortal"
+    console.log("O heroi de nome " + nome + ",esta no nivel " + nivel)    
+}
+
+else if (xp >=10001){
+    let nivel = "Radiante"
+    console.log("O heroi de nome " + nome + ",esta no nivel " + nivel)    
+} else
+    console.log("Sem nivel definido")
+    
+
+
+
+
+
+
