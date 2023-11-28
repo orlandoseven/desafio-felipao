90f10f2e8b054ddaa57fac5ecd8c13ffadad8fe8
orlandoseven <lp.ronnaldo@gmail.com>
2023-11-28 12:02:14 -0300

    projeto versao 1.2

--- index.js
+++ index.js
@@ -1,46 +1,36 @@
-let nome = "Flash"
-let xp = 2001
+let nome = "Homem-aranha"
+let xp = "terra"
+let nivel ="" 
 
 if(xp < 1000){
-    let nivel = "Ferro"
-    console.log("O heroi de nome " + nome + ",esta no nivel " + nivel)    
+    nivel = "Ferro"
 }
-
 else if (xp >= 1001 && xp <= 2000){
-    let nivel = "Bronze"
-    console.log("O heroi de nome " + nome + ",esta no nivel " + nivel)    
+     nivel = "Bronze"
 }
-
 else if (xp >= 2001 && xp <= 6000){
-    let nivel = "Prata"
-    console.log("O heroi de nome " + nome + ",esta no nivel " + nivel)    
+     nivel = "Prata"
 }
-
 else if (xp >= 6001 && xp <= 7000){
-    let nivel = "Ouro"
-    console.log("O heroi de nome " + nome + ",esta no nivel " + nivel)    
+     nivel = "Ouro"
 }
-
 else if (xp >= 7001 && xp <= 8000){
-    let nivel = "Platina"
-    console.log("O heroi de nome " + nome + ",esta no nivel " + nivel)    
+     nivel = "Platina"
 }
-
 else if (xp >= 8001 && xp <= 9000){
-    let nivel = "Ascendente"
-    console.log("O heroi de nome " + nome + ",esta no nivel " + nivel)    
+     nivel = "Ascendente"
 }
-
 else if (xp >= 9001 && xp <= 10000){
-    let nivel = "Imortal"
-    console.log("O heroi de nome " + nome + ",esta no nivel " + nivel)    
+     nivel = "Imortal"
 }
-
 else if (xp >=10001){
-    let nivel = "Radiante"
-    console.log("O heroi de nome " + nome + ",esta no nivel " + nivel)    
-} else
-    console.log("Sem nivel definido")
+     nivel = "Radiante"
+} 
+else {
+     console.log("Nivel não definido")
+}
+console.log("O heroi de nome " + nome + ",esta no nivel " + nivel +"!")    
+
     
 
 
